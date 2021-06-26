0227464254966e53bed8cc920c266568b567aac3
xingyujie <39870096+xingyujie@users.noreply.github.com>
2021-06-26 14:39:09 +0800

    Create gosetup.sh

--- gosetup.sh
+++ gosetup.sh
@@ -1,4 +1,13 @@
 #!/data/data/com.termux/files/usr/bin/bash
+{
+    for ((i = 0 ; i <= 100 ; i+=20)); do
+        sleep 1
+        echo $i
+    done
+} | whiptail --gauge "FlyOS安装程序正在启动..." 6 60 0
+　　
+whiptail --title "FlyOS Installer" --msgbox " 欢迎使用FlyOS安装向导，单击OK开始安装FlyOS，体验极客生活！" 10 60
+whiptail --title "安装询问" --msgbox " 确定安装FlyOS？安装FlyOS操作系统会删除所有程序及数据(包括用户文档和文件等)" 10 60
 echo "安装将会清除所有数据, 请确认已备份重要数据"
 read -p "按下回车继续" _
 echo "[*] 安装依赖"
@@ -16,4 +25,11 @@ cd $PREFIX/etc/flyos
 git checkout dev
 git pull
 pip install -r requirements.txt
-echo "[+] 安装完成, 请重启termux"
+{
+    for ((i = 0 ; i <= 100 ; i+=20)); do
+        sleep 1
+        echo $i
+    done
+} | whiptail --gauge "安装结束，系统自动退出，请稍后" 6 60 0
+　　
+pkill bash
